7bc764b14a28bec85aaf9b71d8a589641a5006e8
atila-tokyo <atilamanager@gmail.com>
2021-11-02 21:31:08 +0900

    Adding dependencies fix, config files, deletions

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React from "react";
+import TextField, { Input } from "@material/react-text-field";
 
-const Home = () => <div> Hello World</div>;
+import { Container, Search } from "./style";
+import logo from "../../assets/logo.svg";
+
+const Home = () => (
+  <Container>
+    <Search>
+            <img src={logo} alt="Website logo" />
+            <TextField
+                label="Search"
+
+            ></TextField>
+    </Search>
+  </Container>
+);
 
 export default Home;
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.aside`
     background-color: ${(props) => props.theme.colors.background};
     width: 360px;
     height: 100vh;
     overflow-y: auto;
+`;
+
+export const Search = styled.section`
+    display: flex;
+    flex-direction: column;
+    background-color: #4E4F50;
+    color: #B1B1B1;
+    padding: 16px;
 `;
