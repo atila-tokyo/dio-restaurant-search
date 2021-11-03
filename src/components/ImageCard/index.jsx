9b20f4f901fb2d7118f1e8c2caa21b5b1030fea3
atila-tokyo <atilamanager@gmail.com>
2021-11-03 15:28:19 +0900

    Adding working Carousel

--- src/components/ImageCard/index.jsx
+++ src/components/ImageCard/index.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import styled from "styled-components"
 
 const Card = styled.div`
+    display: flex;
+    justify-content: center;
     width: 90px;
     height: 90px;border-radius 3px;
-    background-image: ${(props) => props.photo};
+    background-image: url(${(props) => props.photo});
     background-size: cover;
+    margin-top: 5px;
+    padding: 10px;
 `;
 
-const ImageCard = ({ photo }) => <Card photo={photo} />;
+const Title = styled.p`
+    font-family: ${(props) => props.theme.fonts.regular};
+    front-size: 10px;
+`;
+
+const ImageCard = ({ photo, title }) => (
+  <Card photo={photo}>
+    <Title>{title}</Title>
+  </Card>
+  );
 
 export default ImageCard;
