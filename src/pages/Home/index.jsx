972862ef67378d8e61e7c86b2b53714fe9626823
atila-tokyo <atilamanager@gmail.com>
2021-11-03 12:42:27 +0900

    Adding new logo and import of fonts

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 import TextField, { Input } from "@material/react-text-field";
 
-import { Container, Search } from "./style";
+import MaterialIcon from "@material/react-material-icon";
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle } from "./style";
 import logo from "../../assets/logo.svg";
 
 
 const Home = () => {
     const [inputValue, setInputValue] = useState();
     return (
-      <>
+      <Wrapper>
         <Container>
           <Search>
-            <img src={logo} alt="Website logo" />
-            <TextField label="Search" outlined>
+            <Logo src={logo} alt="Website logo" />
+            <TextField
+              label="Search for your next dine"
+              outlined
+              trailingIcon={<MaterialIcon role="button" icon="search" />}
+            >
               <Input value={inputValue} onChange={(e) => setInputValue(e.target.value )} />
             </TextField>
+            <CarouselTitle> Near your location</CarouselTitle>
           </Search>
         </Container>
-      </>
+        <Map />
+      </Wrapper>
     );
 }
 export default Home;
