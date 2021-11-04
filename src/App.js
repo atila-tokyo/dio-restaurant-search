4e1e0cc9a396f703546960cf68d24a258213cda0
atila-tokyo <atilamanager@gmail.com>
2021-11-04 12:20:19 +0900

    Adding initial redux architecture/config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Provider } from "react-redux";
 import { ThemeProvider } from 'styled-components';
 import { Reset } from 'styled-reset';
 
 import Home from 'src/pages/Home';
+import store from "./redux/store";
 import theme from './theme'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
         <Reset />
         <Home />
-      </>
-    </ThemeProvider>
+      
+      </ThemeProvider>
+    </Provider>
   );
 }
 
